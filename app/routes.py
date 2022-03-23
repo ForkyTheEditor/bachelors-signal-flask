c773bed8c25d37d2d43eb8bed54a6c2323bf685d
Alex Forky <alex.farcas.campian@gmail.com>
2022-03-23 19:19:47 +0200

    Added templating

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Salutarile mele!"
+    user = {'username': 'Bai'}
+    posts = [
+        {
+            'author': {'username': 'UnBoacter'},
+            'body': 'Cat e ceasu!'
+        },
+        {
+            'author': {'username': 'UnFraier'},
+            'body': '!'
+        }
+    ]
+    return render_template('index.html', title='Homepage', user=user, posts=posts)
