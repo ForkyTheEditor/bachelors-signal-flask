ad3deb9cbf81595166870d5cc1525f5b656c02b5
Alex Forky <alex.farcas.campian@gmail.com>
2022-04-04 20:04:40 +0300

    Added firebase integration

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,17 @@
+import firebase_admin
 from flask import Flask
 from config import Config
+from firebase_admin import credentials
+from firebase_admin import db
 
 app = Flask(__name__)
 app.config.from_object(Config)
+cred = credentials.Certificate(app.config['FIREBASE_SECRET_KEY_PATH'])
+
+firebase_admin.initialize_app(cred, {
+    'databaseURL': app.config['DB_URL']
+})
+
+db_ref = db.reference()
 
 from app import routes
